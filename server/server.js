207b3c44679460e49105e3bef79c35c1be37b96c
david-lipschitz <david@mypowerstation.biz>
2018-09-25 18:24:05 +0200

    Add connect and disconnect event handlers in socket.io

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 require('./config/config');
 
+const http = require('http'); // no NPM install required because path is built into npm 
 const path = require('path'); // no NPM install required because path is built into npm
 const express = require('express');
-const bodyParser = require('body-parser');
+const socketIO = require('socket.io');
+//const bodyParser = require('body-parser');
 
 const publicPath = path.join(__dirname, '/../public'); // so that we can use /server and /public from the server directory
 
@@ -10,18 +12,27 @@ const publicPath = path.join(__dirname, '/../public'); // so that we can use /se
 // console.log(__dirname + '/../public'); //this is the old way, but we can use path
 // console.log(publicPath);
 
-var app = express();
 const port = process.env.PORT;
+var app = express();
+var server = http.createServer(app); // we are going to modify the way app works with the html library that we have loaded
+var io = socketIO(server); //allows us to emit or listen to events
 
-app.use(bodyParser.json()); //the middleware we need to give to express
+//app.use(bodyParser.json()); //the middleware we need to give to express
 //app.use(express.static(__dirname + '/../public')); -- mine
 app.use(express.static(publicPath)); //Andrew's version
 
-// app.get('/', function(req, res){
-//     res.render('/index.html');
-// });
+//register an event listener: this first event is to listen to a connection
+io.on('connection', (socket) => {
+    console.log('New user connected');
 
-app.listen(port, () => {
+    socket.on('disconnect', () => {
+        console.log('Disconnected from client; user was disconnected');
+    });
+});
+
+//was app.listen... and it became server.listen...
+// and try http://localhost:3000/socket.io/socket.io.js
+server.listen(port, () => {
     console.log(`Started up on port ${port}`);
 });
 
